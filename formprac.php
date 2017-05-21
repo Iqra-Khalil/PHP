f2cd97f616c9c4d9d92e581b962ad841d4f8b06b
Iqra Khalil <ikk_9@yahoo.com>
2017-05-21 04:34:51 -0700

    Add files via upload

---
+++ formprac.php
@@ -0,0 +1,23 @@
+<?php
+include_once('newcon.php');
+if(isset($_POST['submit']))
+{
+$name=$_POST['stdname'];
+$fname=$_POST['fname'];
+mysql_query("insert into userinfo(name,fname)VALUES ('$name','$fname')");
+}
+?>
+
+<html>
+<head>
+</head>
+<body>
+<form action = "" method="post">
+id:<input type="text" name="id" />
+name:<input type="text" name="stdname" />
+fname:<input type="text" name="fname" />
+<input type="submit" name="submit">
+</form>
+
+</body>
+</html>
