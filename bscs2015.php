8d2b4e2e9b148d421829630dbe7a99e68d4199df
Iqra Khalil <ikk_9@yahoo.com>
2017-05-21 05:01:12 -0700

    Add files via upload

---
+++ bscs2015.php
@@ -0,0 +1,27 @@
+<?php
+$name1 = "hello";
+$name2 = "test";
+echo $name1.$name2;
+function myfunction(){
+echo "helloworld";
+}
+$f="myfunction";
+$f();
+
+//echo "<br>".str_repeat("&nbsp;",20)."php switch";
+
+
+echo $_POST['value']; 
+
+
+
+?>
+<form method="post" action="">
+<input type="text" name="value">
+<input type="submit">
+</form>
+
+
+
+
+
